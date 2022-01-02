b97d024274b6f6abc7941bf296b9301f5795e907
suvarchala12 <bhumasuvarchala55@gmail.com>
2022-01-02 11:27:50 +0530

    uc2 insertion

--- Program.cs
+++ Program.cs
@@ -7,15 +7,9 @@ namespace DataStructures
         static void Main(string[] args)
         {
             Console.WriteLine("Data Structure!");
+            string[] names = { "One", "Two", "Four", "Six" };
             DataStructure dataStructure = new DataStructure();
-            string[] words = { "decs", "read", "logic", "search" };
-            string data = "search";
-            int result = dataStructure.FindWord(words, data);
-            if (result == -1)
-                Console.WriteLine("Word not present");
-
-            else
-                Console.WriteLine("Word is found at " + result);
+            dataStructure.Insertion(names);
         }
     }
 }

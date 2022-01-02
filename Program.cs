a289c62bb61c89ce7c513a50587a8a35c657fafa
suvarchala12 <bhumasuvarchala55@gmail.com>
2022-01-02 11:24:50 +0530

    uc1 binarysearch

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("DataStructure Programmes");
+﻿using System;
+
+namespace DataStructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Data Structure!");
+            DataStructure dataStructure = new DataStructure();
+            string[] words = { "decs", "read", "logic", "search" };
+            string data = "search";
+            int result = dataStructure.FindWord(words, data);
+            if (result == -1)
+                Console.WriteLine("Word not present");
+
+            else
+                Console.WriteLine("Word is found at " + result);
+        }
+    }
+}
